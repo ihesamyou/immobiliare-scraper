c77c70b58d638e022d065a31a8b95a2b19f6a035
Hossein <ihosseinu@gmail.com>
2023-11-13 00:24:40 +0100

    add save_data_csv method

--- scraper/scraper.py
+++ scraper/scraper.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import re
 from urllib.parse import urlparse, parse_qs, urlencode, urlunparse
 import json
+import csv
+import pandas as pd
 
 
 class Immobiliare:
@@ -12,6 +14,7 @@ class Immobiliare:
         self.response = requests.get(self.url)
         self._check_url()
         self.json_data = self.gather_json_data()
+        self.data_frame = pd.DataFrame(self.real_estates)
 
     def __str__(self) -> str:
         return f"Immobiliare scraper - url='{self.url}'"
@@ -29,7 +32,7 @@ class Immobiliare:
         if self.response.status_code != 200:
             self.response.raise_for_status()
 
-    def gather_json_data(self):
+    def gather_json_data(self) -> None:
         self.real_estates = []
         self.last_scraped_url = self.url
 
@@ -58,7 +61,7 @@ class Immobiliare:
             print(f"Getting real estate data of {self.url}")
             self.real_estates += self.filter_json_data(self.response)
 
-    def filter_json_data(self, response) -> dict:
+    def filter_json_data(self, response) -> list:
         soup = BeautifulSoup(response.text, 'html.parser')
         json_data = json.loads(soup.find("script", {"id": "__NEXT_DATA__"}).text)
         json_data = json_data["props"]["pageProps"]["dehydratedState"]["queries"][0]["state"]["data"]["pages"][0]["results"]
@@ -69,23 +72,23 @@ class Immobiliare:
             real_estate["id"] = record["realEstate"]["id"]
             real_estate["url"] = record["seo"]["url"]
             real_estate["contract"] = record["realEstate"]["contract"]
-            real_estate["is_private_ad"] = record["realEstate"]["advertiser"].get("agency", None) == None
+            real_estate["agency_id"], real_estate["agency_url"], real_estate["agency_name"] =  None, None, None
+            real_estate["is_private_ad"] = 1 if record["realEstate"]["advertiser"].get("agency", None) == None else 0
             if not real_estate["is_private_ad"]:
                 real_estate["agency_id"] =  record["realEstate"]["advertiser"]["agency"]["label"]
                 real_estate["agency_url"] = record["realEstate"]["advertiser"]["agency"]["agencyUrl"]
                 real_estate["agency_name"] = record["realEstate"]["advertiser"]["agency"]["displayName"]
-            else:
-                real_estate["agency_id"], real_estate["agency_url"], real_estate["agency_name"] =  None, None, None
-            real_estate["is_new"] = record["realEstate"]["isNew"]
-            real_estate["is_luxury"] = record["realEstate"]["luxury"]
+            real_estate["is_new"] = 1 if record["realEstate"]["isNew"] else 0
+            real_estate["is_luxury"] = 1 if record["realEstate"]["luxury"] else 0
             real_estate["formatted_price"] = record["realEstate"]["price"]["formattedValue"]
             real_estate["price"] = record["realEstate"]["price"].get("value", None)
-            if not real_estate["price"]:
+            if (not real_estate["price"]):
                 price_match = re.search(r'\d+\.?\d*', real_estate["formatted_price"])
                 if price_match:
                     real_estate["price"] = price_match.group(0).replace('.', '')
             real_estate["bathrooms"] = record["realEstate"]["properties"][0].get("bathrooms", None)
             real_estate["bedrooms"] = record["realEstate"]["properties"][0].get("bedRoomsNumber", None)
+            real_estate["floor"], real_estate["formatted_floor"] = None, None
             floor_data = record["realEstate"]["properties"][0].get("floor", None)
             if floor_data:
                 real_estate["floor"] = floor_data["abbreviation"]
@@ -93,7 +96,8 @@ class Immobiliare:
             real_estate["total_floors"] = record["realEstate"]["properties"][0].get("floors", None)
             real_estate["condition"] = record["realEstate"]["properties"][0].get("condition", None)
             real_estate["rooms"] = record["realEstate"]["properties"][0].get("rooms", None)
-            real_estate["has_elevators"] = record["realEstate"]["properties"][0]["hasElevators"]
+            real_estate["has_elevators"] = 1 if record["realEstate"]["properties"][0]["hasElevators"] else 0
+            real_estate["surface"] = None
             real_estate["surface_formatted"] = record["realEstate"]["properties"][0].get("surface", None)
             if real_estate["surface_formatted"]:
                 real_estate["surface"] = re.search(r'(\d+\.?\d*)', real_estate["surface_formatted"]).group(1) or None
@@ -102,6 +106,7 @@ class Immobiliare:
             real_estate["category"] = record["realEstate"]["properties"][0]["category"]["name"]
             real_estate["description"] = record["realEstate"]["properties"][0].get("description", None)
             energy_data = record["realEstate"]["properties"][0].get("energy", None)
+            real_estate["heating_type"], real_estate["air_conditioning"] = None, None
             if energy_data:
                 real_estate["heating_type"] = energy_data.get("heatingType", None)
                 real_estate["air_conditioning"] = energy_data.get("airConditioning", None)
@@ -116,10 +121,21 @@ class Immobiliare:
             real_estates.append(real_estate)
         return real_estates
     
-    def save_data_json(self) -> json:
-        with open("immobiliare.json", "w") as file:
+    def save_data_json(self, filename="immobiliare.json") -> None:
+        with open(filename, "w") as file:
             json.dump(self.real_estates, file, indent=4)
+
+    def save_data_csv(self, filename="immobiliare.csv") -> None:
+        with open(filename, "w", newline="", encoding="utf-8") as csvfile:
+            fieldnames = self.real_estates[0].keys() if self.real_estates else []
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writeheader()
+            for real_estate in self.real_estates:
+                writer.writerow(real_estate)
 
 
 immo = Immobiliare(url="https://www.immobiliare.it/affitto-case/milano/?criterio=rilevanza", get_data_of_following_pages=True)
 immo.save_data_json()
+immo.save_data_csv()
+data = immo.data_frame
+print(data)
